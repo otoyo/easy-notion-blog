e2beffbb4e37630187c5578c6c313760f71bcd68
otoyo <hiroki.t.1988@gmail.com>
2022-01-27 17:59:45 +0900

    Pass caption to component

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -243,6 +243,7 @@ const RenderPost = ({
                   <components.Code
                     key={block.Id}
                     language={block.Language || ''}
+                    caption={block.Code.Caption}
                   >
                     {block.Code.Text.map(
                       richText => richText.Text.Content
