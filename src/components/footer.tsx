ff9fde688e2ae65f10a43446388563b825ec3556
otoyo <hiroki.t.1988@gmail.com>
2022-02-01 22:40:01 +0900

    Refine components

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,16 @@
 import ExtLink from './ext-link'
 
+import styles from '../styles/footer.module.css'
+
 const Footer = () => (
-  <>
-    <footer>
-      <div>
-        <ExtLink href="https://github.com/otoyo/easy-notion-blog">
-          This Notion Blog is powered by otoyo/easy-notion-blog
-        </ExtLink>
-      </div>
-    </footer>
-  </>
+  <footer className={styles.footer}>
+    <div>
+      <span>Powered by </span>
+      <ExtLink href="https://github.com/otoyo/easy-notion-blog">
+        easy-notion-blog
+      </ExtLink>
+    </div>
+  </footer>
 )
 
 export default Footer
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,93 +1,38 @@
 import Link from 'next/link'
-import Head from 'next/head'
 import { useRouter } from 'next/router'
 
-import ExtLink from './ext-link'
+import { NEXT_PUBLIC_URL } from '../lib/notion/server-constants'
+
+import { SITE_TITLE } from './document-head'
 import styles from '../styles/header.module.css'
 
-const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
-  { label: 'Blog', page: '/blog' },
-]
-const defaultUrl = 'https://easy-notion-blog-02.vercel.app'
-const defaultTitle = 'herohoroブログ'
-const defaultOgImageUrl = 'https://easy-notion-blog-02.vercel.app/hero-room.jpg'
-const defaultDescription =
-  'This Notion Blog is powered by otoyo/easy-notion-blog'
+interface NavItem {
+  label: string
+  path: string
+}
 
-const Header = ({
-  path = '',
-  titlePre = '',
-  description = '',
-  ogImageUrl = '',
-}) => {
-  const { pathname } = useRouter()
+const Header = () => {
+  const { asPath } = useRouter()
+  const url = new URL(asPath, NEXT_PUBLIC_URL)
+
+  const navItems: NavItem[] = [
+    { label: 'Home', path: '/' },
+    { label: 'Blog', path: '/blog' },
+  ]
 
   return (
     <header className={styles.header}>
-      <Head>
-        <title>
-          {!titlePre ? defaultTitle : `${titlePre} - ${defaultTitle}`}
-        </title>
-        <meta
-          name="description"
-          content={!description ? defaultDescription : description}
-        />
-        <meta
-          property="og:title"
-          content={!titlePre ? defaultTitle : titlePre}
-        />
-        <meta
-          property="og:description"
-          content={!description ? defaultDescription : description}
-        />
-
-        <meta
-          property="og:image"
-          content={ogImageUrl ? ogImageUrl : defaultOgImageUrl}
-        />
-        <meta
-          property="twitter:image"
-          content={ogImageUrl ? ogImageUrl : defaultOgImageUrl}
-        />
-        <meta
-          name="twitter:card"
-          content={!ogImageUrl ? 'summary' : 'summary_large_image'}
-        />
-        <link
-          rel="alternate"
-          type="application/atom+xml"
-          href="/atom"
-          title={defaultTitle}
-        />
-      </Head>
-      <p>
-        <Link href="https://sparkling-cinnamon-3f9.notion.site/herohoro-48ff806d05484215b51b9dc79df15357">
-          ⭐このブログを便利に使う方法⭐
-        </Link>
-      </p>
       <h1>
         <Link href="/" passHref>
-          <a>{defaultTitle}</a>
+          <a>{SITE_TITLE}</a>
         </Link>
       </h1>
-      <p>
-        <Link href="https://easy-notion-blog-02.vercel.app/blog/tag/easy-notion-blog_%E4%BA%8B%E5%A7%8B%E3%82%81">
-          ⭐　easy-notion-blog導入　⭐
-        </Link>
-      </p>
       <ul>
-        {navItems.map(({ label, page, link }) => (
+        {navItems.map(({ label, path }) => (
           <li key={label}>
-            {page ? (
-              <Link href={page} passHref>
-                <a className={pathname === page ? 'active' : undefined}>
-                  {label}
-                </a>
-              </Link>
-            ) : (
-              <ExtLink href={link}>{label}</ExtLink>
-            )}
+            <Link href={path} passHref>
+              <a className={url.pathname === path ? 'active' : null}>{label}</a>
+            </Link>
           </li>
         ))}
       </ul>
