d1c8bea38c4b6b9d98d54fa2f16973b6291a9ec5
otoyo <hiroki.t.1988@gmail.com>
2022-02-18 12:05:25 +0900

    Stub d3.min.js

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,11 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+
+  moduleNameMapper: {
+    // ref. https://github.com/facebook/jest/issues/12036#issuecomment-979219710
+    d3: '<rootDir>/node_modules/d3/dist/d3.min.js',
+  },
 }
 
 module.exports = createJestConfig(customJestConfig)
