6521a3050425ece715e2bee46ec9be7b1eb9868e
nitaking <10850034+nitaking@users.noreply.github.com>
2022-02-20 14:24:01 +0900

    :sparkles: Add default NEXT_PUBLIC_URL

--- src/lib/notion/server-constants.js
+++ src/lib/notion/server-constants.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
-const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
+const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL || 'http://localhost:3000/'
 const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
