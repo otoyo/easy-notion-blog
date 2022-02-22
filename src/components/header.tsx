3804f2e7dfdef3fc890e22442b0606c8f518f01a
herohoro <mtwd.office.25@gmain.com>
2022-02-23 05:18:50 +0900

    コメントアウト site_title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
-import { SITE_TITLE } from './document-head'
+// import { SITE_TITLE } from './document-head'
 import styles from '../styles/header.module.css'
 
 interface NavItem {
