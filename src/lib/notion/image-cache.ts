a970a04b2409d864c1efdbb981eb76cabf7e1094
otoyo <hiroki.t.1988@gmail.com>
2022-02-22 23:14:27 +0900

    Define image-cache

---
+++ src/lib/notion/image-cache.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs'
+
+export const store = async (id: string, url: string) => {
+  const blob = await fetchImageAsBlob(url)
+
+  if (!blob) {
+    return
+  }
+
+  const binary = (await blob.arrayBuffer()) as Uint8Array
+  const buffer = Buffer.from(binary)
+
+  fs.writeFile(filePath(id), buffer, err => {
+    if (err) {
+      console.log(err)
+    }
+  })
+}
+
+const dir = 'public/notion-images'
+
+const filePath = (id: string) => {
+  return `${dir}/${id}.png`
+}
+
+const fetchImageAsBlob = async (url: string) => {
+  try {
+    return await fetch(url).then(res => res.blob())
+  } catch (err) {
+    console.log(err)
+    return null
+  }
+}
