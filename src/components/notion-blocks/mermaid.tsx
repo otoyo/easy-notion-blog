11f2a095d9f5dd18f9d0a8a6e117b46548b7878c
otoyo <hiroki.t.1988@gmail.com>
2022-02-26 15:00:52 +0900

    Move modules

--- src/components/notion-blocks/mermaid.tsx
+++ src/components/notion-blocks/mermaid.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, FC } from 'react'
 import mermaid from 'mermaid'
 
-import styles from '../styles/mermaid.module.css'
+import styles from '../../styles/mermaid.module.css'
 
 type Props = {
   id: string
