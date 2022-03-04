14a4accd442796d2cf56d340ff46334196b78513
otoyo <hiroki.t.1988@gmail.com>
2022-03-04 10:45:52 +0900

    Fix unique logic

--- src/lib/notion/client.ts
+++ src/lib/notion/client.ts
@@ -511,7 +511,7 @@ function _uniqueConditions(conditions = []) {
   const properties = []
 
   return conditions.filter(cond => {
-    if (conditions.includes(cond.property)) {
+    if (properties.includes(cond.property)) {
       return false
     }
     properties.push(cond.property)
