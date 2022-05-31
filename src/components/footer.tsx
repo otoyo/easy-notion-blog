217acc2fb15dd3e269640019ff6c0c4b71a65be2
herohoro <mtwd.office.25@gmail.com>
2022-05-31 13:16:48 +0900

    add footer-logoTitle

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,10 +4,10 @@ import styles from '../styles/footer.module.css'
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <div>
+    <div className={styles.powered}>
       <span>Powered by </span>
       <ExtLink href="https://github.com/otoyo/easy-notion-blog">
-        easy-notion-blog
+        <img src="/title.png"  className={styles.titleLogo}/>
       </ExtLink>
     </div>
   </footer>
