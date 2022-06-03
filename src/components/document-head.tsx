0fe736406d558da5bac238f149810c8ca15884b1
herohoro <mtwd.office.25@gmail.com>
2022-06-03 15:41:40 +0900

    本家コードに戻す

--- src/components/document-head.tsx
+++ src/components/document-head.tsx
@@ -8,8 +8,8 @@ export const SITE_DESCRIPTION =
   '非エンジニアがeasy-notion-blogを通して勉強しながらスキルアップをしていくブログ'
 
 const DocumentHead = ({ title = '', description = '', urlOgImage = '' }) => {
-  const { asPath, pathname } = useRouter()
-  const currentURL = new URL(asPath, NEXT_PUBLIC_URL)
+  const { asPath } = useRouter()
+  // const currentURL = new URL(asPath, NEXT_PUBLIC_URL)
   // const defaultImageURL = 'https://herohoro.com/hero-room.jpg'
   const defaultImageURL = new URL('/hero-room.jpg', NEXT_PUBLIC_URL)
 
@@ -20,34 +20,41 @@ const DocumentHead = ({ title = '', description = '', urlOgImage = '' }) => {
         name="description"
         content={description ? description : SITE_DESCRIPTION}
       />
-      <meta property="og:url" content={currentURL.toString()} />
-
+      
+      {/* ## 最新版コード */}
+      {NEXT_PUBLIC_URL ? (
+        <meta
+          property="og:url"
+          content={new URL(asPath, NEXT_PUBLIC_URL).toString()}
+        />
+      ) : null}
       <meta property="og:title" content={title ? title : SITE_TITLE} />
       <meta
         property="og:description"
         content={description ? description : SITE_DESCRIPTION}
       />
-
-      {/* 
-      ## 最新版コード
       {urlOgImage ? (
         <meta property="og:image" content={urlOgImage} />
       ) : NEXT_PUBLIC_URL ? (
-        <meta property="og:image" content={defaultImageURL.toString()} />
+        <meta
+          property="og:image"
+          content={defaultImageURL.toString()}
+        />
       ) : null}
-      <meta name="twitter:card" content="summary_large_image" />
+
+        <meta name="twitter:card" content="summary_large_image" />
       {urlOgImage ? (
         <meta name="twitter:image" content={urlOgImage} />
       ) : NEXT_PUBLIC_URL ? (
-        <meta name="twitter:image" content={defaultImageURL.toString()} />
-      ) : null} */}
-
-      <meta
-        property="og:image"
-        content={urlOgImage ? urlOgImage : defaultImageURL.toString()}
-      />
+        <meta
+          name="twitter:image"
+          content={defaultImageURL.toString()}
+        />
+      ) : null}
+      
 
       <meta name="twitter:site" content="@mineral_30" />
+      {/* カード大小をしたかったらONにする
       <meta
         name="twitter:card"
         content={
@@ -55,13 +62,16 @@ const DocumentHead = ({ title = '', description = '', urlOgImage = '' }) => {
             ? 'summary_large_image'
             : 'summary'
         }
-      />
-      <meta
-        name="twitter:image"
-        content={urlOgImage ? urlOgImage : defaultImageURL.toString()}
-      />
+      /> */}
+      
 
-      <link rel="canonical" href={currentURL.toString()} />
+      {/* <link rel="canonical" href={currentURL.toString()} /> */}
+      {NEXT_PUBLIC_URL ? (
+        <link
+          rel="canonical"
+          href={new URL(asPath, NEXT_PUBLIC_URL).toString()}
+        />
+      ) : null}
 
       {/* ## これは何だ？(▼ 参照commit)
       https://github.com/otoyo/notion-blog/blob/93213ddbaea359e5177e4ef18bab4c047e20444d/src/components/document-head.tsx#L44
