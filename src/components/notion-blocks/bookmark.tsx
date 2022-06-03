92f39a7fd239c215f2d629f06eb2aa91cdeda360
herohoro <mtwd.office.25@gmail.com>
2022-06-04 04:44:49 +0900

    change My記事リンク_bookmark

--- src/components/notion-blocks/bookmark.tsx
+++ src/components/notion-blocks/bookmark.tsx
@@ -1,6 +1,7 @@
 import { LinkPreview } from '@dhaiwat10/react-link-preview'
 import { NEXT_PUBLIC_URL } from '../../lib/notion/server-constants'
 import styles from '../../styles/notion-block.module.css'
+import Link from 'next/link'
 
 const Bookmark = ({ block }) => {
   const url = block.Bookmark ? block.Bookmark.Url : block.LinkPreview.Url
@@ -10,18 +11,22 @@ const Bookmark = ({ block }) => {
     if(result !== -1){
       // 見つかった時
       return (
-        <LinkPreview
-      url={url}
-      className={styles.linkPreview}
-      fallbackImageSrc={new URL('/error.png', NEXT_PUBLIC_URL).toString()}
-      backgroundColor="#d5d1ed"
+        <div className={styles.MylinkPreview}>
+          <Link href={url.toString()} passHref >
+            <p>へろほろブログの記事はこちら</p>
+          </Link>
+        </div>
+      // url={url}
+      // className={styles.MylinkPreview}
+      // fallbackImageSrc={new URL('/error.png', NEXT_PUBLIC_URL).toString()}
+      // backgroundColor="#d5d1ed"
       // fallbackImageSrc="https://herohoro.com/default.png"
-      imageHeight='120wh'
+      // imageHeight='120wh'
       
-      borderColor='#8c8676'
-      descriptionLength={60}
+      // borderColor='#8c8676'
+      // descriptionLength={60}
       
-    />
+    
       )
     }
     else{
