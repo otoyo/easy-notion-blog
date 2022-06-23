a97c428b3fc8f1829cf5629e8f098f44ed4c2f32
otoyo <hiroki.t.1988@gmail.com>
2022-06-23 17:40:26 +0900

    Define helper methods

--- src/lib/blog-helpers.ts
+++ src/lib/blog-helpers.ts
@@ -36,3 +36,38 @@ export const normalizeSlug = slug => {
   }
   return startingSlash || endingSlash ? normalizeSlug(slug) : slug
 }
+
+export const isYouTubeURL = (url: URL): boolean => {
+  if (['www.youtube.com', 'youtu.be'].includes(url.hostname)) {
+    return true
+  }
+  return false
+}
+
+// Supported URL
+//
+// - http://youtu.be/0zM3nApSvMg
+// - http://www.youtube.com/watch?v=0zM3nApSvMg&feature=feedrec_grec_index
+// - http://www.youtube.com/watch?v=0zM3nApSvMg#t=0m10s
+// - http://www.youtube.com/watch?v=0zM3nApSvMg
+// - http://www.youtube.com/v/0zM3nApSvMg?fs=1&amp;hl=en_US&amp;rel=0
+// - http://www.youtube.com/embed/0zM3nApSvMg?rel=0
+export const parseYouTubeVideoId = (url: URL): string => {
+  if (!isYouTubeURL(url)) return ''
+
+  if (url.hostname === 'youtu.be') {
+    return url.pathname.split('/')[1]
+  } else if (url.pathname === '/watch') {
+    return url.searchParams.get('v')
+  } else {
+    const elements = url.pathname.split('/')
+
+    if (elements.length < 2) return ''
+
+    if (elements[1] === 'v' || elements[1] === 'embed') {
+      return elements[2]
+    }
+  }
+
+  return ''
+}
