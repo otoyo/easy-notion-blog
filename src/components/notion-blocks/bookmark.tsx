6578b7a87d404356351ad3fd3d23ef778c2f7d1b
otoyo <hiroki.t.1988@gmail.com>
2022-08-16 22:16:07 +0900

    Reform Bookmark component

--- src/components/notion-blocks/bookmark.tsx
+++ src/components/notion-blocks/bookmark.tsx
@@ -1,16 +1,72 @@
-import { LinkPreview } from '@dhaiwat10/react-link-preview'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import styles from '../../styles/notion-block.module.css'
 
+interface Metadata {
+  title: string | null
+  description: string | null
+  image: string | null
+}
+
 const Bookmark = ({ block }) => {
-  const url = block.Bookmark ? block.Bookmark.Url : block.LinkPreview.Url
+  let sURL: string | null
+  if (block.Bookmark) {
+    sURL = block.Bookmark.Url
+  } else if (block.LinkPreview) {
+    sURL = block.LinkPreview.Url
+  } else if (block.Embed) {
+    sURL = block.Embed.Url
+  }
+
+  const [metadata, setMetadata] = useState<Metadata | null>()
+
+  useEffect(() => {
+    try {
+      const url = new URL(sURL)
+      axios.get(`/api/url-metadata?url=${url.toString()}`)
+        .then((res) => {
+          setMetadata(res.data as Metadata)
+        })
+    } catch (e) {
+      console.log(e)
+    }
+  }, [sURL])
+
+  let url: URL
+  try {
+    url = new URL(sURL)
+  } catch (e) {
+    console.log(e)
+  }
+
+  if (!metadata || !url) {
+    return <></>
+  }
+
+  const { title, description, image } = metadata
 
   return (
-    <LinkPreview
-      url={url}
-      className={styles.linkPreview}
-      descriptionLength={60}
-    />
+    <div className={styles.bookmark}>
+      <a href={url.toString()} target="_blank" rel="noopener noreferrer">
+        <div>
+          <div>{title ? title : ''}</div>
+          <div>{description ? description : ''}</div>
+          <div>
+            <div>
+              <img src={`http://www.google.com/s2/favicons?domain=${url.hostname}`} alt="title" loading="lazy" decoding="async" />
+            </div>
+            <div>{url.origin}</div>
+          </div>
+        </div>
+        <div>
+          {image
+            ? <img src={image} alt="title" loading="lazy" decoding="async" />
+            : null
+          }
+        </div>
+      </a>
+    </div>
   )
 }
 
