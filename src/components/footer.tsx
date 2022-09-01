863e73cd18a9310826627f15d0da18d30261bd19
Thales <hi@anhdung.me>
2022-09-01 20:40:43 +0700

    Try to change something

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,10 @@ import styles from '../styles/footer.module.css'
 const Footer = () => (
   <footer className={styles.footer}>
     <div>
-      <span>Powered by </span>
+      <ExtLink href="https://thocode.net">
+        Thợ Code
+      </ExtLink>
+      <span>. Powered by </span>
       <ExtLink href="https://github.com/otoyo/easy-notion-blog">
         easy-notion-blog
       </ExtLink>
