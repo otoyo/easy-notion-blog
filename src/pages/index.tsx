3bc96d1e410592dfaef13d4bc283c456d6c6b90f
Thales <hi@anhdung.me>
2022-09-02 13:32:56 +0700

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import DocumentHead from '../components/document-head'
+import { SITE_TITLE, SITE_DESCRIPTION, DocumentHead } from '../components/document-head'
 import ExtLink from '../components/ext-link'
 import styles from '../styles/page.module.css'
 
@@ -7,15 +7,11 @@ const RenderPage = () => (
     <DocumentHead />
 
     <div>
-      <h2>Welcome!</h2>
-      <p>Your easy-notion-blog deployed successfully!</p>
-      <p>Have fun!</p>
-      <p>
-        easy-notion-blog powered by{' '}
-        <ExtLink href="https://github.com/otoyo/easy-notion-blog">
-          otoyo/easy-notion-blog
-        </ExtLink>
-      </p>
+      <h2>Hello, world!</h2>
+      <p>Lời quê góp nhặt dông dài</p>
+      <p>Mua vui cũng được một trống canh</p>
+      <br />
+      <p>${SITE_TITLE}, ${SITE_DESCRIPTION}</p>
     </div>
   </div>
 )
