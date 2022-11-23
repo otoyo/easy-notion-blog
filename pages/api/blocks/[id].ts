dd3a280034ad5f58271c2a36450baed2d797f4a8
otoyo <hiroki.t.1988@gmail.com>
2022-11-23 11:02:11 +0900

    Define /api/blocks/[id]

---
+++ pages/api/blocks/[id].ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { getBlock } from '../../../lib/notion/client'
+
+const ApiBlock = async function(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    res.statusCode = 400
+    res.end()
+    return
+  }
+
+  const { id: ids } = req.query
+
+  if (!ids) {
+    res.statusCode = 400
+    res.end()
+    return
+  }
+
+  const id = ids.toString()
+
+  try {
+    const block = await getBlock(id)
+
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify(block))
+    res.statusCode = 200
+    res.end()
+  } catch (e) {
+    console.log(e)
+    res.statusCode = 500
+    res.end()
+  }
+}
+
+export default ApiBlock
