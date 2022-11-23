fb026a2007ee3900162e63ee26a11242daf7e59c
otoyo <hiroki.t.1988@gmail.com>
2022-11-23 16:18:45 +0900

    Define BlogSlugHead

---
+++ app/blog/[slug]/head.tsx
@@ -0,0 +1,19 @@
+import { NEXT_PUBLIC_URL } from '../../server-constants'
+import { getPostBySlug } from '../../../lib/notion/client'
+import { getBlogLink } from '../../../lib/blog-helpers'
+import DocumentHead from '../../../components/document-head'
+
+const BlogSlugHead = async ({ params: { slug } }) => {
+  const post = await getPostBySlug(slug)
+
+  return (
+    <DocumentHead
+      title={post.Title}
+      description={post.Excerpt}
+      path={getBlogLink(post.Slug)}
+      urlOgImage={NEXT_PUBLIC_URL && post.OGImage && new URL(`/api/og-image/${post.Slug}`, NEXT_PUBLIC_URL).toString()}
+    />
+  )
+}
+
+export default BlogSlugHead
