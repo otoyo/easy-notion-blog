a78d21ad53859bdb8624ad1f533e584ac87be846
otoyo <hiroki.t.1988@gmail.com>
2022-12-24 14:23:50 +0900

    Add og:type

--- components/document-head.tsx
+++ components/document-head.tsx
@@ -4,50 +4,63 @@ import {
   NEXT_PUBLIC_SITE_DESCRIPTION,
 } from '../app/server-constants'
 
-const DocumentHead = ({ title = '', description = '', path = '', urlOgImage = '' }) => (
-  <>
-    <title>{title ? `${title} - ${NEXT_PUBLIC_SITE_TITLE}` : NEXT_PUBLIC_SITE_TITLE}</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-    <meta
-      name="description"
-      content={description ? description : NEXT_PUBLIC_SITE_DESCRIPTION}
-    />
-    {NEXT_PUBLIC_URL ? (
+const DocumentHead = ({ title = '', description = '', path = '', urlOgImage = '' }) => {
+  const elements = path.split('/')
+  const isSlugPath = elements[0] === '' && elements[1] === 'blog' && elements.length === 3
+  const isRootPath = path === '' || path === '/'
+
+  return (
+    <>
+      <title>{title ? `${title} - ${NEXT_PUBLIC_SITE_TITLE}` : NEXT_PUBLIC_SITE_TITLE}</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
       <meta
-        property="og:url"
-        content={new URL(path, NEXT_PUBLIC_URL).toString()}
+        name="description"
+        content={description ? description : NEXT_PUBLIC_SITE_DESCRIPTION}
       />
-    ) : null}
-    <meta property="og:title" content={title ? title : NEXT_PUBLIC_SITE_TITLE} />
-    <meta
-      property="og:description"
-      content={description ? description : NEXT_PUBLIC_SITE_DESCRIPTION}
-    />
-    <meta property="og:site_name" content={NEXT_PUBLIC_SITE_TITLE} />
-    {urlOgImage ? (
-      <meta property="og:image" content={urlOgImage} />
-    ) : NEXT_PUBLIC_URL ? (
+      {NEXT_PUBLIC_URL ? (
+        <meta
+          property="og:url"
+          content={new URL(path, NEXT_PUBLIC_URL).toString()}
+        />
+      ) : null}
+      <meta property="og:title" content={title ? title : NEXT_PUBLIC_SITE_TITLE} />
       <meta
-        property="og:image"
-        content={new URL('/default.png', NEXT_PUBLIC_URL).toString()}
+        property="og:description"
+        content={description ? description : NEXT_PUBLIC_SITE_DESCRIPTION}
       />
-    ) : null}
-    <meta name="twitter:card" content="summary_large_image" />
-    {urlOgImage ? (
-      <meta name="twitter:image" content={urlOgImage} />
-    ) : NEXT_PUBLIC_URL ? (
-      <meta
-        name="twitter:image"
-        content={new URL('/default.png', NEXT_PUBLIC_URL).toString()}
-      />
-    ) : null}
-    {NEXT_PUBLIC_URL ? (
-      <link
-        rel="canonical"
-        href={new URL(path, NEXT_PUBLIC_URL).toString()}
-      />
-    ) : null}
-  </>
-)
+      <meta property="og:site_name" content={NEXT_PUBLIC_SITE_TITLE} />
+      <meta property="og:type" content={
+          isRootPath
+          ? 'website'
+          : isSlugPath
+            ? 'article'
+        : 'blog'
+      } />
+      {urlOgImage ? (
+        <meta property="og:image" content={urlOgImage} />
+      ) : NEXT_PUBLIC_URL ? (
+        <meta
+          property="og:image"
+          content={new URL('/default.png', NEXT_PUBLIC_URL).toString()}
+        />
+      ) : null}
+      <meta name="twitter:card" content="summary_large_image" />
+      {urlOgImage ? (
+        <meta name="twitter:image" content={urlOgImage} />
+      ) : NEXT_PUBLIC_URL ? (
+        <meta
+          name="twitter:image"
+          content={new URL('/default.png', NEXT_PUBLIC_URL).toString()}
+        />
+      ) : null}
+      {NEXT_PUBLIC_URL ? (
+        <link
+          rel="canonical"
+          href={new URL(path, NEXT_PUBLIC_URL).toString()}
+        />
+      ) : null}
+    </>
+  )
+}
 
 export default DocumentHead
