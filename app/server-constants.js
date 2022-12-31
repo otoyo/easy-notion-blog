e168a73fac3baa73e253f68903bdf9b73b96949a
otoyo <hiroki.t.1988@gmail.com>
2022-12-31 15:29:55 +0900

    Remove path from server-constants.js

--- app/server-constants.js
+++ app/server-constants.js
@@ -9,7 +9,6 @@ const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
 const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 const NEXT_PUBLIC_SITE_TITLE = 'easy-notion-blog'
 const NEXT_PUBLIC_SITE_DESCRIPTION = 'Easy to start your blog. You can write on your Notion.'
-const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
 
 module.exports = {
@@ -19,6 +18,5 @@ module.exports = {
   NEXT_PUBLIC_GA_TRACKING_ID,
   NEXT_PUBLIC_SITE_TITLE,
   NEXT_PUBLIC_SITE_DESCRIPTION,
-  BLOG_INDEX_CACHE,
   NUMBER_OF_POSTS_PER_PAGE,
 }
