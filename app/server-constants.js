9317e5e54ee10d6588a5e5ab0d81837f87835c9e
otoyo <hiroki.t.1988@gmail.com>
2023-01-02 17:22:29 +0900

    Move path module include to blog-index-cache.js

--- app/server-constants.js
+++ app/server-constants.js
@@ -1,15 +1,12 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
 // use commonjs so it can be required without transpiling
 
-const path = require('path')
-
 const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
 const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 const NEXT_PUBLIC_SITE_TITLE = 'easy-notion-blog'
 const NEXT_PUBLIC_SITE_DESCRIPTION = 'Easy to start your blog. You can write on your Notion.'
-const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
 
 module.exports = {
@@ -19,6 +16,5 @@ module.exports = {
   NEXT_PUBLIC_GA_TRACKING_ID,
   NEXT_PUBLIC_SITE_TITLE,
   NEXT_PUBLIC_SITE_DESCRIPTION,
-  BLOG_INDEX_CACHE,
   NUMBER_OF_POSTS_PER_PAGE,
 }
