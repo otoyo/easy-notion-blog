3ce29c375dd719d12d8cb51d8a232d455131b075
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-01-08 17:09:05 +0900

    fix: error handling with unfounded post

--- app/blog/[slug]/head.tsx
+++ app/blog/[slug]/head.tsx
@@ -5,12 +5,14 @@ import DocumentHead from '../../../components/document-head'
 const BlogSlugHead = async ({ params: { slug } }) => {
   const post = await getPostBySlug(slug)
 
-  return (
+  return post ? (
     <DocumentHead
       title={post.Title}
       description={post.Excerpt}
       path={getBlogLink(post.Slug)}
     />
+  ) : (
+    <DocumentHead/>
   )
 }
 
